ea711d29c75b4d7fad3bb34e74463396d888922d
Matthew Swartwout <mwswartwout@gmail.com>
2016-02-16 21:57:28 -0500

    Adds another quaternion function

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import math
 
+from geometry_msgs.msg import Quaternion
+
 
 # Function returns the sign of a number
 def sgn(x):
@@ -23,3 +25,12 @@ def min_spin(spin_angle):
 # Converts from Quaternion to yaw
 def convert_planar_quaternion_to_phi(quaternion):
     return 2 * math.atan2(quaternion.z, quaternion.w)
+
+
+def convert_planar_phi_to_quaternion(phi):
+    quaternion = Quaternion()
+    quaternion.x = 0
+    quaternion.y = 0
+    quaternion.z = math.sin(phi / 2)
+    quaternion.w = math.cos(phi / 2)
+    return quaternion
